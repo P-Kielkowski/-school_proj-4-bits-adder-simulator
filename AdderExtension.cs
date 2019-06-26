5d173a608700cbfd9847baf57e7c37ea4296f002
Paweł Kiełkowski <pawel.kielkowski17@gmail.com>
2019-06-26 20:08:42 +0200

    commit nr 2 (school task)

---
+++ AdderExtension.cs
@@ -0,0 +1,18 @@
+﻿using Sumator.Code;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sumator
+{
+	public static class AdderExtension
+	{
+
+		public static void SwitchOutput(this Adder adder)
+		{
+			adder.cOut = !adder.cOut;
+		}
+	}
+}
